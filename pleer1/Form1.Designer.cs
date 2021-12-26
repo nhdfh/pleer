2eb32484da841e562392ccbf5e8c52176252e351
nhdfh <nhdfh@DESKTOP-Q3OPSRO>
2021-12-26 14:58:58 +0500

    Добавьте файлы проекта.

---
+++ pleer1/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace pleer1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 100);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Открыть файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 160);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Play";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(93, 160);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Pause";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(174, 160);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Stop";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(218, 8);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(45, 104);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "0:0";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 131);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(237, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(227, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "0:0";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(118, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(259, 190);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Player";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ pleer1/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using Windows.Storage.FileProperties;
+
+namespace pleer1
+{
+    public partial class Form1 : Form
+    {
+        
+        private WaveOutEvent outputDevice;
+        private AudioFileReader audioFile;
+        
+        string file = "G:/osu!/Songs/[DOT96 obj：皿魔人] SAMBISTA[皿]/audio.mp3";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            outputDevice = new WaveOutEvent();
+            audioFile = new AudioFileReader(file);
+            
+            var closing = false;
+            outputDevice.PlaybackStopped += (s, a) => { if (closing) { outputDevice.Dispose(); audioFile.Dispose(); } };
+            outputDevice.Init(audioFile);
+            trackBar1.Value =(int) (audioFile.Volume*100f);
+            //var f = new Form();
+            //var b = new Button() { Text = "Play" };
+            //b.Click += (s, a) => outputDevice.Play();
+            //var b2 = new Button() { Text = "Stop", Left = b.Right };
+            //b2.Click += (s, a) => outputDevice.Stop();
+            //var b3 = new Button { Text = "Rewind", Left = b2.Right };
+            //b3.Click += (s, a) => audioFile.Position = 0;
+            //var b4 = new Button { Text = "Open", Left = b3.Right };
+            //b4.Click += new EventHandler(Open_file);
+            //var t = new TrackBar() { Minimum = 0, Maximum = 100, Value = 100, Top = b.Bottom, TickFrequency = 10 };
+            //t.Scroll += (s, a) => outputDevice.Volume = t.Value / 100f;
+            //// Alternative: t.Scroll += (s, a) => audioFile.Volume = t.Value / 100f;
+            //f.Controls.AddRange(new Control[] { b, b2, b3, b4, t });
+            //f.FormClosing += (s, a) => { closing = true; outputDevice.Stop(); };
+            //f.ShowDialog();
+        }
+        //private void Open_file(object sender, EventArgs e)
+        //{
+
+        //    var filePath = string.Empty;
+
+        //    using (OpenFileDialog openFileDialog = new OpenFileDialog())
+        //    {
+
+        //        openFileDialog.RestoreDirectory = true;
+
+        //        if (openFileDialog.ShowDialog() == DialogResult.OK)
+        //        {
+        //            //Get the path of specified file
+        //            filePath = openFileDialog.FileName;
+        //            file = filePath;
+        //            audioFile = new AudioFileReader(file);
+        //            outputDevice = new WaveOutEvent();
+        //            outputDevice.Init(audioFile);
+        //        }
+                
+
+        //    }
+
+        //}
+        //private void OnButtonPlayClick(object sender, EventArgs args)
+        //{
+        //    if (outputDevice == null)
+        //    {
+        //        outputDevice = new WaveOutEvent();
+        //        outputDevice.PlaybackStopped += OnPlaybackStopped;
+        //    }
+        //    if (audioFile == null)
+        //    {
+        //        audioFile = new AudioFileReader(@"D:\example.mp3");
+        //        outputDevice.Init(audioFile);
+        //    }
+        //    outputDevice.Play();
+        //}
+        //private void OnButtonStopClick(object sender, EventArgs args)
+        //{
+        //    outputDevice?.Stop();
+        //}
+        //private void OnPlaybackStopped(object sender, StoppedEventArgs args)
+        //{
+        //    outputDevice.Dispose();
+        //    outputDevice = null;
+        //    audioFile.Dispose();
+        //    audioFile = null;
+        //}
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+                   
+                    file = filePath;
+                    audioFile = new AudioFileReader(file);
+                    outputDevice.Stop();
+                    outputDevice.Init(audioFile);
+                    TagLib.File file_TAG = TagLib.File.Create(file);
+                    if (file_TAG.Tag.Pictures.Length >= 1)
+                    {
+                        var bin = (byte[])(file_TAG.Tag.Pictures[0].Data.Data);
+                        pictureBox1.Image = Image.FromStream(new MemoryStream(bin));
+                       
+                    }
+                    this.Text = audioFile.FileName.Split('\\').Last();
+                }
+
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+       
+            outputDevice.Play();
+            label1.Text = audioFile.CurrentTime.ToString();
+            label2.Text = audioFile.TotalTime.Minutes.ToString() + ":" + audioFile.TotalTime.Seconds.ToString();
+            progressBar1.Maximum = (int)audioFile.TotalTime.TotalSeconds;
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            outputDevice.Stop();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            audioFile.Position = 0;
+            outputDevice.Stop();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            outputDevice.Volume = trackBar1.Value / 100f;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Value = (int)audioFile.CurrentTime.TotalSeconds;
+            label1.Text = audioFile.CurrentTime.Minutes.ToString() + ":" + audioFile.CurrentTime.Seconds.ToString();
+        }
+    }
+}
